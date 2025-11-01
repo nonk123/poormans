c941ea972035f3cef388102c877463cc854278ff
nonk123 <me@nonk.dev>
2025-11-01 08:38:07 +0300

    Nuke some stale toys

--- src/platformer/blocks.c
+++
@@ -1,9 +0,0 @@
-#include "terrain.h"
-
-extern const BlockTable TB_DIRT;
-const BlockTable TB_AIR = {0};
-
-const BlockTable* Blocks[BLOCK_MAX] = {
-	[BLOCK_AIR] = &TB_AIR,
-	[BLOCK_DIRT] = &TB_DIRT,
-};
